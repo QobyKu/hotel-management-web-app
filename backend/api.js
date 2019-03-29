4867619adf1bdc8ff383383604df2c54fe62f943
Pranav <pnarang1130@gmail.com>
2019-03-28 17:29:55 -0700

    returning id in response

--- backend/api.js
+++ backend/api.js
@@ -55,11 +55,11 @@ app.post('/signup', function (req, res) {
     var zip_code = req.body.zipCode;
 
 
-    connection.query(` INSERT INTO Customer(Name, PhoneNumber, StAddress, ZipCode, UserName, Password) VALUES ('${name}', '${phone_number}', '${street_address}', '${zip_code}', '${user_name}', '${password}'); `, function (error, results, fields) {
+    connection.query(` INSERT INTO Customer(Name, PhoneNumber, StAddress, ZipCode, UserName, Password) VALUES ('${name}', '${phone_number}', '${street_address}', '${zip_code}', '${user_name}', '${password}');`, function (error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
-
+    
 });
 
 // Find Rooms
